18ece097bc47b050aca7f0614991191a5a3c4ca1
Idris.S <kiteshi.ssh@gmail.com>
2024-10-03 01:40:44 +0300

    fix: update codegen config

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   schema: process.env.API_URL || "http://127.0.0.1:8080/query",
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
-  // documents: ["**/*.{ts,tsx}"],
+  documents: ["**/*.{ts,tsx}"],
   generates: {
     "./graphql/": {
       preset: "client",
--- graphql/graphql.ts
+++ graphql/graphql.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
@@ -59,3 +60,13 @@ export type User = {
   id: Scalars['ID']['output'];
   updatedAt: Scalars['Time']['output'];
 };
+
+export type RegisterUserMutationVariables = Exact<{
+  input: RegisterUser;
+}>;
+
+
+export type RegisterUserMutation = { __typename?: 'Mutation', registerUser: number };
+
+
+export const RegisterUserDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"RegisterUser"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"input"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"RegisterUser"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"registerUser"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"input"},"value":{"kind":"Variable","name":{"kind":"Name","value":"input"}}}]}]}}]} as unknown as DocumentNode<RegisterUserMutation, RegisterUserMutationVariables>;
